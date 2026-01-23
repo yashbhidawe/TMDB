677a0e453290f51cc67234374df6ee49454e45ac
Yash! <ybh5179@gmail.com>
2026-01-23 09:21:57 +0530

    initialize react native with expo and nativewind

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-accent">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
